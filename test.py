9c1e7a556c974b744835bb96bd2aff285fa2a0e4
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2023-08-20 18:49:00 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import cv2
 frame1 = (cv2.imread('img.jpg', cv2.IMREAD_GRAYSCALE))
 frame2 = (cv2.imread('img.jpg', cv2.IMREAD_GRAYSCALE))
 print(frame1.shape)
-nvof = cv2.cuda_NvidiaOpticalFlow_1_0.create(frame1.shape[1], frame1.shape[0], 5, False, False, False, 3)
+nvof = cv2.cuda_NvidiaOpticalFlow_1_0.create((frame1.shape[1], frame1.shape[0]), 5, False, False, False, 3)
 
 flow = nvof.calc(frame1, frame2, None)
 
